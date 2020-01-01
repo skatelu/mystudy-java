6dae6265410fa1febee8aa93fdaf12f98d53a1f1
skatelu_home <865470994@qq.com>
2020-01-02 00:35:28 +0800

    update jvm

--- java-class/src/com/study/javaclass/jvm_lecture/jvm/classloader/MyTest1_2.java
+++ java-class/src/com/study/javaclass/jvm_lecture/jvm/classloader/MyTest1_2.java
@@ -10,6 +10,7 @@ package com.study.javaclass.jvm_lecture.jvm.classloader;
  * 在这个类中 子类调用的是子类的静态属性  这种情况称为 对MyParent1_2，MyChild1_2的主动使用  MyChild1_2会进行初始化
  * MyChild1进行初始化，静态代码块就会进行执行
  * 当一个类在初始化时，要求其父类全部都已经初始化完毕了
+ * -XX:+TraceClassLoading,用于追踪类的加载信息并打印出来
  */
 public class MyTest1_2 {
     public static void main(String[] args) {
--- java-class/src/com/study/javaclass/polymorphism/CarDemo.java
+++ java-class/src/com/study/javaclass/polymorphism/CarDemo.java
@@ -1,5 +1,7 @@
 package com.study.javaclass.polymorphism;
 
+import com.study.javaclass.SedanCar;
+
 public class CarDemo extends SedanCar {
 
 }
