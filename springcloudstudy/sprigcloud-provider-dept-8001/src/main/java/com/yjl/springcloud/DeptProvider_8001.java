6462b92e97fbc0669e96fe8352dc3933da95eb98
skatelu <865470994@qq.com>
2020-08-26 07:32:26 +0800

    上传springCloud

--- springcloudstudy/sprigcloud-provider-dept-8001/src/main/java/com/yjl/springcloud/DeptProvider_8001.java
+++ springcloudstudy/sprigcloud-provider-dept-8001/src/main/java/com/yjl/springcloud/DeptProvider_8001.java
@@ -2,8 +2,12 @@ package com.yjl.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient  // 开启Eureka 客户端，在服务启动后自动注册到Eureka中
+@EnableDiscoveryClient
 public class DeptProvider_8001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_8001.class, args);
